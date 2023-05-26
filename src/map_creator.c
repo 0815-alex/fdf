0b49170268c58909898cb7d464bfb1ce7a371bc9
astein <stein.alexander@posteo.de>
2023-05-26 21:28:19 +0100

    -> Makefile Commit <-

--- src/map_creator.c
+++ src/map_creator.c
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 01:07:33 by astein            #+#    #+#             */
-/*   Updated: 2023/05/26 06:37:44 by astein           ###   ########.fr       */
+/*   Updated: 2023/05/26 21:19:42 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,141 +19,63 @@ typedef struct s_fd
 
 }				t_fd;
 
-char	*create_map(char *str)
+static void	insert_empty_char(void)
 {
-	t_list	**fds;
-	t_list	*cur_fd;
-	char	*cur_letter;
-	char	cur_filename[100];
-	int		new_file_fd;
-	char	*new_file_name;
-	char	*str_backup;
-	char	*cur_line;
-	t_list	*temp;
-	t_bool	eof;
+	printf("TO DOOO HERE SHOULD OPEN A EMPTY FILE\n");
+}
 
-	printf("NEW MAP CREATE START: %s\n", str);
-	str_backup = str;
-	//looped durch den string
-	fds = NULL;
+static char	*create_new_file(char *str, int *new_fd)
+{
+	char	*new_file;
+
+	new_file = malloc(sizeof(char) * (ft_strlen(str) + ft_strlen(PATH_2_CHARS)
+				+ 20));
+	new_file[0] = '\0';
+	ft_strlcat(new_file, PATH_2_NAMES, ft_strlen(PATH_2_NAMES) + 1);
+	ft_strlcat(new_file, str, ft_strlen(new_file) + ft_strlen(str) + 1);
+	ft_strlcat(new_file, ".fdf", ft_strlen(new_file) + 5);
+	printf("new file path: %s\n", new_file);
+	*new_fd = open(new_file, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	return (new_file);
+}
+
+static void	create_fd_list(t_list **fds, char *str)
+{
+	char	*cur_letter;
+	t_list	*cur_fd;
+	char	cur_filename[100];
+
 	cur_filename[0] = '\0';
 	cur_letter = malloc(sizeof(char) * 2);
-	fds = malloc(sizeof(t_list *));
-	*fds = NULL;
-	// cur_fd = malloc(sizeof(t_fd));
-	// ((t_fd *)cur_fd)->next = NULL;
-	// ft_strlcat(cur_filename, PATH_2_CHARS, ft_strlen(PATH_2_CHARS) + 1);
-	// ft_strlcat(cur_filename, "space", ft_strlen(cur_filename) + 6);
-	// ft_strlcat(cur_filename, ".fdf", ft_strlen(cur_filename) + 5);
-	// ((t_fd *)cur_fd)->fd = open(&cur_filename[0], O_RDONLY);
-	// printf("open file: %s | fd=%d\n", cur_filename, ((t_fd *)cur_fd)->fd);
-	// ft_bzero(cur_filename, 100);
-	// ft_lstadd_back(fds, cur_fd);
+	insert_empty_char();
 	while (*str)
 	{
-		// *str = ft_toupper(*str);
+		cur_fd = malloc(sizeof(t_fd));
+		((t_fd *)cur_fd)->next = NULL;
+		ft_strlcat(cur_filename, PATH_2_CHARS, ft_strlen(PATH_2_CHARS) + 1);
 		if (ft_isalnum(*str))
 		{
-			//oeffnet die datein und speichert die fd in einer liste
-			cur_fd = malloc(sizeof(t_fd));
-			((t_fd *)cur_fd)->next = NULL;
-			ft_strlcat(cur_filename, PATH_2_CHARS, ft_strlen(PATH_2_CHARS) + 1);
 			ft_strlcpy(cur_letter, &str[0], 2);
-			printf("cur letter: %s\n", cur_letter);
 			ft_strlcat(cur_filename, cur_letter, ft_strlen(cur_filename) + 2);
-			ft_strlcat(cur_filename, ".fdf", ft_strlen(cur_filename) + 5);
-			((t_fd *)cur_fd)->fd = open(&cur_filename[0], O_RDONLY);
-			printf("open file: %s | fd=%d\n", cur_filename,
-					((t_fd *)cur_fd)->fd);
-			ft_lstadd_back(fds, cur_fd);
-			ft_bzero(cur_filename, 100);
-			cur_fd = malloc(sizeof(t_fd));
-			((t_fd *)cur_fd)->next = NULL;
-			ft_strlcat(cur_filename, PATH_2_CHARS, ft_strlen(PATH_2_CHARS) + 1);
-			ft_strlcpy(cur_letter, &str[0], 2);
-			printf("cur letter: %s\n", cur_letter);
-			ft_strlcat(cur_filename, cur_letter, ft_strlen(cur_filename) + 2);
-			ft_strlcat(cur_filename, ".fdf", ft_strlen(cur_filename) + 5);
-			((t_fd *)cur_fd)->fd = open(&cur_filename[0], O_RDONLY);
-			printf("open file: %s | fd=%d\n", cur_filename,
-					((t_fd *)cur_fd)->fd);
-			ft_lstadd_back(fds, cur_fd);
-			ft_bzero(cur_filename, 100);
-            ft_bzero(cur_letter,2);
 		}
 		else
-		{
-			cur_fd = malloc(sizeof(t_fd));
-			((t_fd *)cur_fd)->next = NULL;
-			ft_strlcat(cur_filename, PATH_2_CHARS, ft_strlen(PATH_2_CHARS) + 1);
-			ft_strlcat(cur_filename, "questionmark", ft_strlen(cur_filename) + 13);
-			ft_strlcat(cur_filename, ".fdf", ft_strlen(cur_filename) + 5);
-			((t_fd *)cur_fd)->fd = open(&cur_filename[0], O_RDONLY);
-			printf("open file: %s | fd=%d\n", cur_filename,
-					((t_fd *)cur_fd)->fd);
-			ft_bzero(cur_filename, 100);
-			ft_lstadd_back(fds, cur_fd);
-			str[0] = '?';
-		}
+			ft_strlcat(cur_filename, "qm", ft_strlen(cur_filename) + 3);
+		ft_strlcat(cur_filename, ".fdf", ft_strlen(cur_filename) + 5);
+		((t_fd *)cur_fd)->fd = open(&cur_filename[0], O_RDONLY);
+		printf("open file: %s | fd=%d\n", cur_filename, ((t_fd *)cur_fd)->fd);
+		ft_lstadd_back(fds, cur_fd);
+		ft_bzero(cur_filename, 100);
 		str++;
 	}
-    // cur_fd = malloc(sizeof(t_fd));
-    // ((t_fd *)cur_fd)->next = NULL;
-    // ft_bzero(cur_filename, 100);
-    // ft_strlcat(cur_filename, PATH_2_CHARS, ft_strlen(PATH_2_CHARS) + 1);
-    // ft_strlcat(cur_filename, "space", ft_strlen(cur_filename) + 6);
-    // ft_strlcat(cur_filename, ".fdf", ft_strlen(cur_filename) + 5);
-    // ((t_fd *)cur_fd)->fd = open(&cur_filename[0], O_RDONLY);
-    // printf("open file: %s | fd=%d\n", cur_filename, ((t_fd *)cur_fd)->fd);
-    // ft_lstadd_back(fds, cur_fd);
-    // cur_fd = malloc(sizeof(t_fd));
-    // ((t_fd *)cur_fd)->next = NULL;
-    // ft_bzero(cur_filename, 100);
-    // ft_strlcat(cur_filename, PATH_2_CHARS, ft_strlen(PATH_2_CHARS) + 1);
-    // ft_strlcat(cur_filename, "space", ft_strlen(cur_filename) + 6);
-    // ft_strlcat(cur_filename, ".fdf", ft_strlen(cur_filename) + 5);
-    // ((t_fd *)cur_fd)->fd = open(&cur_filename[0], O_RDONLY);
-    // printf("open file: %s | fd=%d\n", cur_filename, ((t_fd *)cur_fd)->fd);
-    // ft_lstadd_back(fds, cur_fd);
-	printf("all opened!\n");
-	//looped duch die fd
-	new_file_name = malloc(sizeof(char) * (ft_strlen(str)
-				+ ft_strlen(PATH_2_CHARS) + 20));
-	new_file_name[0] = '\0';
-	ft_strlcat(new_file_name, PATH_2_NAMES, ft_strlen(PATH_2_NAMES) + 1);
-	ft_strlcat(new_file_name, str_backup, ft_strlen(new_file_name)
-			+ ft_strlen(str_backup) + 1);
-	ft_strlcat(new_file_name, ".fdf", ft_strlen(new_file_name) + 5);
-	printf("new file path: %s\n", new_file_name);
-	new_file_fd = open(new_file_name, O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	cur_fd = *fds;
-	eof = ft_false;
-	while (cur_fd && eof == ft_false)
-	{
-		printf("read from fd: %d\n", ((t_fd *)cur_fd)->fd);
-		cur_line = get_next_line(((t_fd *)cur_fd)->fd);
-		printf("read = %s\n", cur_line);
-		if (cur_line == NULL)
-			eof = ft_true;
-		else
-			write(new_file_fd, cur_line, ft_strlen(cur_line));
-		free(cur_line);
-		cur_fd = ((t_list *)cur_fd)->next;
-			write(new_file_fd, " ", 1);
-		if (!cur_fd)
-		{
-			write(new_file_fd, "\n", 1);
-			cur_fd = *fds;
-		}
-		
-	}
-	//  liest immer eine zeile
-	//  dann linebreak
-	// add EOF
-	//closed alle fd
-	close(new_file_fd);
-	printf("closed fd new file: %d\n", new_file_fd);
+	insert_empty_char();
+	free(cur_letter);
+}
+
+static void	free_fd_list(t_list **fds)
+{
+	t_list	*cur_fd;
+
 	cur_fd = *fds;
 	while (cur_fd)
 	{
@@ -161,17 +83,52 @@ char	*create_map(char *str)
 		printf("closed fd: %d\n", ((t_fd *)cur_fd)->fd);
 		cur_fd = ((t_list *)cur_fd)->next;
 	}
-	//freed stuff
-	free(cur_letter);
+	cur_fd = *fds;
 	while (*fds)
 	{
-		temp = *fds;
+		cur_fd = *fds;
 		*fds = (*fds)->next;
-		free(temp);
+		free(cur_fd);
 	}
+}
+
+char	*create_map(char *str)
+{
+	t_list	**fds;
+	int		new_fd;
+	char	*new_file;
+	char	*cur_line;
+	char	*cur_line_trimmed;
+	t_list	*cur_fd;
+
+	fds = malloc(sizeof(t_list *));
+	*fds = NULL;
+	create_fd_list(fds, str);
+	new_file = create_new_file(str, &new_fd);
+	//loop through fd list and write a lines
+	cur_fd = *fds;
+	while (cur_fd)
+	{
+		cur_line = get_next_line(((t_fd *)cur_fd)->fd);
+		cur_line_trimmed = ft_strtrim(cur_line, "\n");
+		free(cur_line);
+		if (cur_line_trimmed == NULL)
+			break ;
+		write(new_fd, cur_line_trimmed, ft_strlen(cur_line_trimmed));
+		free(cur_line_trimmed);
+		cur_fd = ((t_list *)cur_fd)->next;
+		if (!cur_fd)
+		{
+			write(new_fd, "\n", 1);
+			cur_fd = *fds;
+		}
+		else
+			write(new_fd, " ", 1);
+	}
+	//closed alle fd
+	close(new_fd);
+	free_fd_list(fds);
 	free(fds);
-	// exit(0);
 	sleep(1);
-	printf("return (new filename: %s\n", new_file_name);
-	return (new_file_name);
+	return (new_file);
 }
